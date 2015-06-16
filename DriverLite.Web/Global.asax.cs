14ccd245cfaf22b49b4cf0502f9d9adbf149feb3
SimonB65 <sbateman@netspace.net.au>
2015-06-16 15:20:24 +1000

    Added status page

--- DriverLite.Web/Global.asax.cs
+++ DriverLite.Web/Global.asax.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Http;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace DriverLite.Web
 {
@@ -13,6 +10,8 @@ namespace DriverLite.Web
 
         protected void Application_Start(object sender, EventArgs e)
         {
+            RouteTable.Routes.MapRoute("Default", "{controller}/{action}", new { controller = "Status", action = "Index" });
+
             GlobalConfiguration.Configuration.Routes.MapHttpRoute(
                  name: "DefaultApi",
                  routeTemplate: "api/{controller}/{id}",
---
+++ DriverLite.Web/Status/StatusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DriverLite.Web.Status
+{
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Content("<html><body>OK! " + DateTimeOffset.Now.ToString("O") + "</body></html>");
+        }
+    }
+}
